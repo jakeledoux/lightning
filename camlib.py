570b45013bdca8c837dc72659711cb6a10e7b9fd
Jake Ledoux <contactjakeledoux@gmail.com>
2020-12-17 18:29:12 -0500

    Removed auto-exposure lol

--- camlib.py
+++ camlib.py
@@ -7,7 +7,7 @@ cam_exp = 0
 cam = None
 
 
-def get_frame(auto_exp=False):
+def get_frame():
     global cam
     global cam_open
     global cam_exp
@@ -19,11 +19,6 @@ def get_frame(auto_exp=False):
 
     # Read frame
     status, frame = cam.read()
-    if auto_exp:
-        exp = sum(cv2.mean(frame))
-        cam_exp += ((exp / 255) - 0.5) * -1
-        cam_exp = min(max(cam_exp, -10), 10)
-        cam.set(cv2.CAP_PROP_EXPOSURE, cam_exp)
     if status:
         return frame
     return False
