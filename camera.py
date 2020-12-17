65adecd892d01d4334bf4a76e1fd1d9f9c124508
Jake Ledoux <contactjakeledoux@gmail.com>
2020-12-17 14:11:56 -0500

    Continuous data stream

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = 1984
 
 
@@ -14,10 +14,12 @@ def get_command(idx):
 
 role = get_command(0)
 if role:
+    # The server will recieve and view the image stream
     if role == 'server':
         print('Listening on {}:{}'.format(HOST, PORT))
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.bind((HOST, PORT))
+            sock.settimeout(5)
             sock.listen()
             conn, addr = sock.accept()
             with conn:
@@ -27,15 +29,16 @@ if role:
                     if not data:
                         print('Connection terminated')
                         break
-                    conn.sendall(data)
+                    print(data)
+    # The client will capture and transmit the image stream
     elif role == 'client':
         address = get_command(1)
         if address:
             print('Attempting connection to {}:{}'.format(address, PORT))
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.connect((address, PORT))
-                sock.sendall(b'Hello, world!')
-                data = sock.recv(1024)
+                while True:
+                    sock.sendall(b'Hello, world!')
             print('Receieved', repr(data))
         else:
             print('No server address specified')
