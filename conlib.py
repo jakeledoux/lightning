7a27c66488fedd7e594b9fa8b1523ed7e77f5d08
Jake Ledoux <contactjakeledoux@gmail.com>
2020-12-17 21:46:36 -0500

    Slight performance improvements

--- conlib.py
+++ conlib.py
@@ -6,42 +6,51 @@ stop_threads = False
 
 class ControlLoop:
     def __init__(self):
-        self.event_handler = threading.Thread(target=update_controls)
+        for device in inputs.devices:
+            if device.name == 'Microsoft X-Box 360 pad':
+                gamepad = device
+                break
+        else:
+            raise Exception('Cannot find Xbox controller. Is it connected?')
+        self.event_handler = threading.Thread(target=update_controls, args=[gamepad])
+        self.event_handler.daemon = True
 
     def __enter__(self):
         self.event_handler.start()
         return self
 
     def __exit__(self, type, value, traceback):
-        global stop_threads
-        stop_threads = True
-        self.event_handler.join()
+        pass
 
     @staticmethod
     def get_controls():
         global controls
         return controls
 
-def update_controls():
+def update_controls(gamepad):
     global controls
 
+    gamepad.read_size = 1
+
     while True:
-        events = inputs.get_gamepad()
-        for event in events:
+        for event in gamepad.read():
             if event.code == 'ABS_X':
-                controls['steer'] = round(event.state / 32768, 2)
+                controls['steer'] = event.state / 32768
             elif event.code == 'ABS_RZ':
-                controls['gas'] = round(event.state / 255, 2)
+                controls['gas'] = event.state / 255
             elif event.code == 'ABS_Z':
-                controls['gas'] = round(-event.state / 255, 2)
+                controls['gas'] = -event.state / 255
             elif event.code == 'BTN_SOUTH':
                 controls['beep'] = event.state
-        global stop_threads
-        if stop_threads:
             break
 
 
 if __name__ == '__main__':
+    import time
+    poll_rate = 20
     with ControlLoop() as controller:
+        last_poll = time.time()
         while True:
-            print(controller.get_controls())
+            if time.time() - last_poll >= 1 / poll_rate:
+                print(controller.get_controls())
+                last_poll = time.time()
